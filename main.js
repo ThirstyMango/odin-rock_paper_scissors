8ad9a98c9d81c9147dc31f5ea4f5502644659510
ThirstyMango <vojtaraja@seznam.cz>
2025-09-16 17:35:23 +0200

    Block user from leaving the game and display thread message

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function getHumanChoice() {
   // The input has to be either rock, paper or scissors. The case does not matter.
   function validateHumanChoice(humanChoice) {
+    if (humanChoice === null) {
+      return false;
+    }
     const humanChoiceLowerCase = humanChoice.toLowerCase();
     const VALID_INPUTS = ["rock", "paper", "scissors"];
     return VALID_INPUTS.includes(humanChoiceLowerCase);
@@ -11,13 +14,19 @@ function getHumanChoice() {
     "Welcome player! \n You have been chosen to play a well know game, Rock, Paper, Scissors against our fellow champion. \n What is your choice?";
   const ERROR_MESSAGE =
     "This input is not supported, please, enter either 'rock', 'paper' or 'scissors'. The case does not matter. ";
+  const THREAD = "You cannot leave until you play!";
 
   // Ask user
   let humanChoice = prompt(HELLO_MESSAGE);
 
-  // Repeat the promt until the input is either rock, paper or scissors
+  // Repeat the promt until the input is either rock, paper or scissors.
   while (!validateHumanChoice(humanChoice)) {
-    humanChoice = prompt(ERROR_MESSAGE);
+    if (humanChoice === null) {
+      // User tried to flee.
+      humanChoice = prompt(THREAD);
+    } else {
+      humanChoice = prompt(ERROR_MESSAGE);
+    }
   }
 
   return humanChoice;
