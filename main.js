907d1527f1955eb423a0fad79b02449acb5ed2e4
ThirstyMango <72852697+ThirstyMango@users.noreply.github.com>
2025-09-18 14:41:45 +0200

    Refactor code via function expression, single ' for strings.

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-function getHumanChoice() {
-  // The input has to be either rock, paper or scissors. The case does not matter.
-  function validateHumanChoice(humanChoice) {
+const getHumanChoice = function getHumanChoice() {
+  
+  // Input is rock | paper | scissors. Any case accepted.
+  const validateHumanChoice = function validateHumanChoice(humanChoice) {
+    
     if (humanChoice === null) {
       return false;
     }
+    
     const humanChoiceLowerCase = humanChoice.toLowerCase();
-    const VALID_INPUTS = ["rock", "paper", "scissors"];
+    const VALID_INPUTS = ['rock', 'paper', 'scissors'];
     return VALID_INPUTS.includes(humanChoiceLowerCase);
   }
 
-  // Promt messages
-  const HELLO_MESSAGE = "What is your choice?";
+  // Prompt messages
+  const HELLO_MESSAGE = 'What is your choice?';
   const ERROR_MESSAGE =
-    "This input is not supported, please, enter either 'rock', 'paper' or 'scissors'. The case does not matter. ";
-  const THREAD = "You cannot leave until you play!";
+    'This input is not supported, please, enter either \'rock\', \'paper\' or \'scissors\'. The case does not matter. ';
+  const THREAD = 'You cannot leave until you play!';
 
   // Ask user
   let humanChoice = prompt(HELLO_MESSAGE);
 
   // Repeat the promt until the input is either rock, paper or scissors.
   while (!validateHumanChoice(humanChoice)) {
-    if (humanChoice === null) {
-      // User tried to flee.
+    if (humanChoice === null) { // User tried to flee.
       humanChoice = prompt(THREAD);
     } else {
       humanChoice = prompt(ERROR_MESSAGE);
@@ -29,106 +31,104 @@ function getHumanChoice() {
   }
 
   return humanChoice;
-} // -> "rock" | "paper" | "scissors"
+} // -> 'rock' | 'paper' | 'scissors'
 
-function getComputerChoice() {
+const getComputerChoice = function getComputerChoice() {
   // Choosing a random valid option from the array.
-  const OPTIONS = ["rock", "paper", "scissors"];
+  const OPTIONS = ['rock', 'paper', 'scissors'];
   const randomIndex = Math.floor(Math.random() * 3);
   const computerChoice = OPTIONS[randomIndex];
   return computerChoice;
-} // -> "rock" | "paper" | "scissors"
+} // -> 'rock' | 'paper' | 'scissors'
 
-function playGame() {
+const playGame = function playGame() {
   const CONFIRM_MESSAGE =
-    "Welcome player! \n You have been chosen to play a well known game, Rock, Paper, Scissors against our fellow champion. \n Do you want to play?";
+    'Welcome player! \n You have been chosen to play a well known game, Rock, Paper, Scissors against our fellow champion. \n Do you want to play?';
 
   // If player does not want to play
   if (!confirm(CONFIRM_MESSAGE)) {
     return;
   }
 
-  const ROUNDS = 5;
-  const enoughToWin = Math.floor(ROUNDS / 2) + 1;
+  const NUMBER_OF_ROUNDS = 5;
+  let currentRound = 1; 
   let humanScore = 0;
   let computerScore = 0;
 
-  function playRound(humanChoice, computerChoice) {
+  const playRound = function playRound(humanChoice, computerChoice) {
+    currentRound++; 
     const humanChoiceLowerCase = humanChoice.toLowerCase();
     let winner = null; // Presume tie
-    let gameState = `A tie! ${humanChoice} does nothing to ${computerChoice}`; // Alert later
+    let gameState = `A tie! ${humanChoice} does nothing to ${computerChoice}`;
 
-    // compare plays, determine winner if no tie, update his score
     switch (humanChoiceLowerCase) {
-      case "rock":
+      case 'rock':
         switch (computerChoice) {
-          case "paper":
+          case 'paper':
             computerScore++;
-            winner = "Computer";
+            winner = 'Computer';
             break;
-          case "scissors":
+          case 'scissors':
             humanScore++;
-            winner = "Human";
+            winner = 'Human';
             break;
         }
         break;
-      case "paper":
+      case 'paper':
         switch (computerChoice) {
-          case "rock":
+          case 'rock':
             humanScore++;
-            winner = "Human";
+            winner = 'Human';
             break;
-          case "scissors":
+          case 'scissors':
             computerScore++;
-            winner = "Computer";
+            winner = 'Computer';
             break;
         }
         break;
-      case "scissors":
+      case 'scissors':
         switch (computerChoice) {
-          case "rock":
+          case 'rock':
             computerScore++;
-            winner = "Computer";
+            winner = 'Computer';
             break;
-          case "paper":
+          case 'paper':
             humanScore++;
-            winner = "Human";
+            winner = 'Human';
             break;
         }
     }
 
-    // If winner, change gameState variable
+    // If winner alerted message has to change 
     if (winner) {
-      const humanState = winner === "Human" ? "win" : "lose";
-      const winSymbol =
-        winner === "Human" ? humanChoice.toLowerCase() : computerChoice;
-      const loseSymbol =
-        winner === "Human" ? computerChoice : humanChoice.toLowerCase();
+      const humanState = (winner === 'Human') ? 'win' : 'lose';
+      const winnerChoice =
+        (winner === 'Human') ? humanChoiceLowerCase : computerChoice;
+      const loserChoice =
+        (winner === 'Human') ? computerChoice : humanChoiceLowerCase;
 
-      gameState = `Computer played ${computerChoice}. You ${humanState}, ${winSymbol} beats ${loseSymbol}. The score is human - ${humanScore}:${computerScore} - computer.`;
+      gameState = `Computer played ${computerChoice}. You ${humanState}, ${winnerChoice} beats ${loserChoice}. The score is human - ${humanScore}:${computerScore} - computer.`;
     }
 
-    alert(gameState); // Notify user of result
+    alert(gameState); 
   } // -> null
 
-  while (humanScore !== enoughToWin && computerScore !== enoughToWin) {
-    // while no winner
-    // getPlays
+  while (currentRound <= NUMBER_OF_ROUNDS) {
+    
     const humanChoice = getHumanChoice();
     const computerChoice = getComputerChoice();
 
-    // And play one round
     playRound(humanChoice, computerChoice);
   }
 
-  // Last message alerted
-  if (humanScore === enoughToWin) {
-    alert(`You have won ${humanScore}:${computerScore}. Congratulations!`);
-    return "Human";
-  }
-
-  alert(`You have lost ${humanScore}:${computerScore}`);
-  return "Computer";
-} // -> str; Winner of the game
+  let resultMessage = `A tie. The score was human - ${humanScore}:${computerScore} - computer.`
+  
+  if (humanScore > computerScore) {
+    resultMessage = `You have won ${humanScore}:${computerScore}. Congratulations!`;
+  }else if(humanScore < computerScore){
+    resultMessage = `You have lost ${humanScore}:${computerScore}. Too bad!`
+  };
+  return alert(resultMessage)
+} // -> null
 
 playGame();
